91e7cbb89ad96f59989c25dffd1385a0fc1c18e1
anuragrao04 <raoanu2004@gmail.com>
2023-12-01 09:32:20 +0530

    idk what commit

--- app/Components/RecipeCard.jsx
+++ app/Components/RecipeCard.jsx
@@ -6,21 +6,32 @@ export default function RecipeCard({ recipe }) {
     const redirect_url = "/recipe/" + recipe._id;
     return (
         <Link href={redirect_url}>
-            {" "}
             <div className="relative flex justify-between align-middle m-5 text-white rounded-2xl bg-red-500 max-w-md ">
                 <div className="m-5">
                     <div className="rounded-full overflow-hidden h-24 w-24 bg-white mx-1">
                         <Image src={recipe.image} alt="Food" />
                     </div>
                 </div>
-                <div className="m-5">
-                    <h1 className="text-3xl font-bold">{recipe.title}</h1>
-                    <p>{recipe.description}</p>
-                </div>
-                <div className="absolute flex flex-row-reverse bottom-5 right-5">
-                    {recipe.ingredients.map((ingredient) => {
-                        <RecipeIngIcon key={ingredient.name} icon={ingredient.image} />;
-                    })}
+
+                <div className="flex flex-col">
+                    <div className="m-5">
+                        <h1 className="text-3xl font-bold">{recipe.title}</h1>
+                        <p>{recipe.description}</p>
+                    </div>
+                    <div className="flex flex-row-reverse pr-5">
+                        {recipe.ingredients.map((ingredient) => {
+                            return (
+                                <div className="" key={ingredient.name}>
+                                    <div className="flex justify-left pl-5 align-middle my-2">
+                                        <div className="rounded-full bg-orange-300 flex p-1">
+                                            <div className="rounded-full h-10 w-10 bg-white  "></div>
+                                            <p className="p-1 mr-1">{ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </Link>
